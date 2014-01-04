6cd2b02eff1b09a3161525ef8bb80ca754b73b17
vagrant <vagrant@rails-dev-box>
2014-01-04 04:20:44 +0000

    First commit and README update

---
+++ README.md
@@ -0,0 +1,3 @@
+# Checkpup
+
+CheckPup software is an easy-to-use tool tailored specifically for a boarding, grooming and/or dog daycare facility. It will make it easy for boarding facilities to check in dogs, track families and view photos of the dogs. 
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Checkpup::Application.config.secret_token = '73bc8db43dca2c3c112ebf762c169c6b269bc31b0a85d2bdd177e0bfa48059adc08c14a1177477fa19dcb4c25dee119e9695e8ea00c04ea0629844584e1100a8'
