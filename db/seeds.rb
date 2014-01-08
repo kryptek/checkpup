9e8c1cddd00b04999f453959c86d1ac2513edf68
vagrant <vagrant@rails-dev-box>
2014-01-08 23:48:42 +0000

    Installed Faker and seeded the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+rand(10..30).times do
+  p = Dog.create(name: Faker::Lorem.words(rand(1..10)).join(" "), 
+    needs: Faker::Lorem.paragraphs(rand(1..4)).join("\n"), 
+    breed: Faker::Lorem.words(rand(1..10)).join(" "))
+end
+
+puts "Seed finished"
+puts "#{Dog.count} posts created"
