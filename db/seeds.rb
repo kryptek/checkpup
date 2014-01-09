eb4c247fcc05a8ab94cb683422c9ca6860cfd029
vagrant <vagrant@rails-dev-box>
2014-01-09 05:16:34 +0000

    Added faker gem and populated seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,8 @@ require 'faker'
 rand(10..30).times do
   p = Dog.create(name: Faker::Lorem.words(rand(1..10)).join(" "), 
     needs: Faker::Lorem.paragraphs(rand(1..4)).join("\n"), 
-    breed: Faker::Lorem.words(rand(1..10)).join(" "))
+    breed: Faker::Lorem.words(rand(1..10)).join(" "),
+    weight: rand(1..100), age: rand(1..100)) 
 end
 
 puts "Seed finished"
