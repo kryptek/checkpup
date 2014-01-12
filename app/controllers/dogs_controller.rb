3b27b5c91e4f8503e54afcf77fc76dc4366637b3
vagrant <vagrant@rails-dev-box>
2014-01-12 02:00:52 +0000

    Added create and update CRUD actions

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -8,8 +8,31 @@ class DogsController < ApplicationController
   end
 
   def new
+    @dog = Dog.new 
+  end
+
+  def create
+  @dog = Dog.new(params[:dog])
+    if @dog.save
+      flash[:notice] = "Dog was created."
+      redirect_to @dog
+    else
+      flash[:error] = "There was an error saving the dog. Please try again."
+      render :new
+    end
   end
 
   def edit
+  end
+
+   def update
+    @dog = Dog.find(params[:id])
+    if @dog.update_attributes(params[:post])
+      flash[:notice] = "Dog was updated."
+      redirect_to @dog
+    else
+      flash[:error] = "There was an error saving the dog. Please try again."
+      render :edit
+    end
   end
 end
