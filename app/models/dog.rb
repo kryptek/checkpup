34a0c82f29af5170a261d6efa8be1e9ebddd2e53
brit200313 <brittany.jill.martin@gmail.com>
2014-01-15 04:12:18 +0000

    Added validation methods to dog

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,4 +2,8 @@ class Dog < ActiveRecord::Base
   has_many :owners
   attr_accessible :age, :breed, :name, :needs, :weight
   belongs_to :user
+
+  validates :name, length: { minimum: 5 }, presence: true
+  validates :needs, length: { minimum: 20 }, presence: true
+  validates :breed, length: { minimum: 5 }, presence: true
 end
